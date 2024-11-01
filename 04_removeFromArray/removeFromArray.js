9e9fe5c35ba32f1d546f6847541a2dc08cb4aeaa
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-01 11:54:25 -0400

    Pass 1st test for removal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(items, removeThis) {
+    let newItems = items;
+    for(let item of items) {
+        if (item === removeThis) {
+            newItems.splice(items.indexOf(item), 1);
+        }
+    }
+    return newItems;
 };
 
 // Do not edit below this line
