793ddd80e1d4679b95f98234dc728a8598fea2fc
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-01 13:55:48 -0400

    Throw error if negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(int1, int2) {
     
-    if (Number.isInteger(int1) && Number.isInteger(int2)) {    
+    if ((Number.isInteger(int1) && Number.isInteger(int2)) && int1 >= 0 && int2 >= 0) {    
         const max = Math.max(int1, int2);
         const min = Math.min(int1, int2);
         let total = 0;
