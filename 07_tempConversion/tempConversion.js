e8f9c260e7839f400842cedfd9f7506ffe62fd84
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-01 14:21:31 -0400

    Create temp converter functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  const cTemp = 5 / 9 * (fTemp - 32);
+  return parseFloat(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const fTemp = 9 / 5 * cTemp + 32;
+  return parseFloat(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
