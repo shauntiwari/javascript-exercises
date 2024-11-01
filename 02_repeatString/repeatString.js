99e69b868beef7dcde1856749134c7804e8e8c21
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-01 11:04:53 -0400

    Pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let finalString = '';
+    
+    for (let i = 1; i <= num; i++) {
+        finalString = finalString + text;
+    }
 
+    return finalString;
 };
+
+repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
