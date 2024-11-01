d7aabf8525239a839c1c941ed50dc4fb81fbc7ca
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-01 13:50:30 -0400

    Pass first test of sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    
+    if (Number.isInteger(int1) && Number.isInteger(int2)) {    
+        const max = Math.max(int1, int2);
+        const min = Math.min(int1, int2);
+        let total = 0;
 
+        for (let i = min; i <= max; i++) {
+            total += i;
+        }
+        return total;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
