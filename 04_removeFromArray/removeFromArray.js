9fd51f6fa28abe8c768b608f2cd3d22c062137e6
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-01 12:49:04 -0400

    Remove unnecessary for loop with filter()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,7 @@
 const removeFromArray = function(items, ...removeThese) {
     let newItems = [...items];  // create a new independent "deep copy" of the array
                                 // so I won't modify the original one
-    for(let item of newItems) {
-        newItems = newItems.filter(item => !removeThese.includes(item));
-    }
+    newItems = newItems.filter(item => !removeThese.includes(item));
     return newItems;
 };
 
