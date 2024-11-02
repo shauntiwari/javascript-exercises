2edf925ded1911317b41918baf187d18333ce407
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-02 19:10:37 -0400

    Updated comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,8 @@
 // Project Plan:
-// 1st step is get rid of any punctuation and make every character lowercase
-// 2nd step is spread the string into an array character by character
-// 3rd step is reverse the array into a new array
-// 4th step is compare the new array to the original array,
-//  if they're exactly equal it's a palindrome, if not, it's not
+// 1st step is get rid of any punctuation, spaces or other irrelevant characters via regex
+// 2nd step is make it all lowercase
+// 3rd step is create a new reversed string
+// 4th step is compare the new string to the original string
 
 
 const palindromes = function(text) {
