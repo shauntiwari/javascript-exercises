fc35d51fc4b33914206a4be808c58a1f36b933e8
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-02 18:40:20 -0400

    Created calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, x) => sum + x, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, x) => product * x, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
