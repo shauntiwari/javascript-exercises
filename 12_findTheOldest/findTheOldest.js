8c91bc8f01f2f692ff83d75120571b8ce53c3695
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-02 22:02:43 -0400

    Completed find the oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, current) => {
+        const currentYear = new Date().getFullYear();
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+        return oldestAge < currentAge ? current : oldest;   
+    }, people[0]);
+    return oldestPerson;
 };
 
 // Do not edit below this line
