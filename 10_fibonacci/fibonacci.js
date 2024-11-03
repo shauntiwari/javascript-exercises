b3304056b6cd32804de339d3533d059117f5c26b
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-11-02 21:13:31 -0400

    Created fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    } else {
+        let fibArray = [0, 1];
+        for (let i = 1; i <= num; i++) {
+            fibArray.push(fibArray[i] + fibArray[i - 1]);
+        }
+        return fibArray[num];
+    }
 };
 
 // Do not edit below this line
